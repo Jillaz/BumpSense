abfea0d94330595b4a259778d6a31126ddbccad1
Jillaz <127788124+Jillaz@users.noreply.github.com>
2026-06-30 09:28:45 +0300

    Revert "refacto2"
    This reverts commit 69f76395d63b422f108b3b20026126de827344b9.

--- app/src/main/java/ru/razrabozavr/bumpsense/presentation/map/SettingsManager.kt
+++ app/src/main/java/ru/razrabozavr/bumpsense/presentation/map/SettingsManager.kt
@@ -30,7 +30,7 @@ class SettingsManager(context: Context) {
     private fun loadSettings(): SettingsState {
         return SettingsState(
             isDarkTheme = appPreferences.isDarkTheme,
-            gpsIntervalSeconds = (appPreferences.gpsUpdateIntervalMs / 1000).toInt(),
+            gpsIntervalSeconds = appPreferences.gpsIntervalSeconds,
             updateRadiusMeters = appPreferences.updateRadiusMeters,
             accelerometerThreshold = appPreferences.accelerometerThreshold,
             autoSaveIntervalMinutes = appPreferences.autoSaveIntervalMinutes,
@@ -52,7 +52,7 @@ class SettingsManager(context: Context) {
     }
 
     fun updateGpsInterval(seconds: Int) {
-        appPreferences.gpsUpdateIntervalMs = seconds * 1000L
+        appPreferences.gpsIntervalSeconds = seconds
         _settingsState.value = _settingsState.value.copy(gpsIntervalSeconds = seconds)
     }
 
